8ac689193f326ef1a6c5a0567a2219c3848c7959
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2022-06-22 15:33:08 +0800

    Delete Examples directory

--- Examples/12_print/print.java
+++
@@ -1,9 +0,0 @@
-import com.spire.doc.Document;
-
-public class print {
-    public static void main(String[] args) {
-        Document document = new Document();
-        document.loadFromFile("data/print.docx");
-        document.getPrintDocument().print();
-    }
-}
